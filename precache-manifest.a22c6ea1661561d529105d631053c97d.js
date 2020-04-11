4f41e05cbf02b4ab9a7fb788434d7987e0652495
Amarendra Sharma <s.amarendra1995@gmail.com>
2020-04-12 04:47:18 +0530

    Initial dist subtree commit

---
+++ precache-manifest.a22c6ea1661561d529105d631053c97d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4676d16870c7fd64e4",
+    "url": "/vwa/css/about.caf2a0a0.css"
+  },
+  {
+    "revision": "017054369cebe25f4917",
+    "url": "/vwa/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "fe5dafa3214a78205b8d2fa9f2b7a38f",
+    "url": "/vwa/index.html"
+  },
+  {
+    "revision": "de4676d16870c7fd64e4",
+    "url": "/vwa/js/about.1404bb81.js"
+  },
+  {
+    "revision": "017054369cebe25f4917",
+    "url": "/vwa/js/app.93646bef.js"
+  },
+  {
+    "revision": "ca1e7bcce51de24f8f1b",
+    "url": "/vwa/js/chunk-vendors.4f04ac9b.js"
+  },
+  {
+    "revision": "bce9a6386358acff4b20d415334d0d28",
+    "url": "/vwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vwa/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,3 @@
+importScripts("/vwa/precache-manifest.a22c6ea1661561d529105d631053c97d.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
